6c6988101a7db961026d61d3ea7306f04cc92068
Jon_China <503665965@qq.com>
2017-12-30 18:12:35 +0800

    feat(common):公共包抽取
    公共包抽取

---
+++ src/main/java/com/cloud/common/dto/BaseRespDTO.java
@@ -0,0 +1,55 @@
+package com.cloud.common.dto;
+
+import com.cloud.common.enums.ResultCode;
+
+import java.io.Serializable;
+
+public class BaseRespDTO implements Serializable {
+    private static final long serialVersionUID = -7964635136197468217L;
+
+    private Serializable data;
+
+    private String code;
+
+    private String message;
+
+
+    public BaseRespDTO() {
+        this(ResultCode.OK);
+    }
+
+    public BaseRespDTO(String code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+    public BaseRespDTO(ResultCode resultCode){
+        this.code = resultCode.getCode();
+        this.message = resultCode.getMessage();
+    }
+
+    public Serializable getData() {
+        return data;
+    }
+
+    public void setData(Serializable data) {
+        this.data = data;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+
+}
